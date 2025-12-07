c91276b56fabdc26ed9fffbf86649dbe206ff409
Vdarak <vdarak.infinity@gmail.com>
2025-12-07 15:50:12 -0600

    fix:updated the speed of the sphere to 0.015

--- components/sentient-sphere.tsx
+++ components/sentient-sphere.tsx
@@ -37,7 +37,7 @@ export const defaultSphereSettings: SphereSettings = {
     code: "#b4f3fd", // updated to user's color
   },
   colorTransitionSpeed: 0.3, // updated to 0.3
-  rotationSpeed: 0.01,
+  rotationSpeed: 0.015,
   rotationDirection: -1,
   rotationAxis: "xy",
   colorMode: "gradient",
